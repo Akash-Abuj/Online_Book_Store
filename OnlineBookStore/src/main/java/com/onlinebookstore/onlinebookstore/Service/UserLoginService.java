d106c717f9247cff7c22e19b7c6d6ef08015420f
Akash Abuj <akashabuj0818@gmail.com>
2024-03-22 14:09:42 +0530

    first commit

---
+++ OnlineBookStore/src/main/java/com/onlinebookstore/onlinebookstore/Service/UserLoginService.java
@@ -0,0 +1,9 @@
+package com.onlinebookstore.onlinebookstore.Service;
+
+import com.onlinebookstore.onlinebookstore.Entities.UserLogin;
+
+public interface UserLoginService {
+String signup(UserLogin userLogin);
+String login(String email, String password);
+UserLogin getByEmail(String email);
+}
